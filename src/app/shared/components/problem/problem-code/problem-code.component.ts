62961abfc50276e22f9b0bd68a6d08152f655bff
Anthony McLamb <adylanmclamb@yahoo.com>
2023-10-09 22:42:17 -0400

    Setting up problem solving

--- src/app/shared/components/problem/problem-code/problem-code.component.ts
+++ src/app/shared/components/problem/problem-code/problem-code.component.ts
@@ -1,10 +1,19 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
+import { DomSanitizer, SafeHtml } from '@angular/platform-browser';
 
 @Component({
   selector: 'app-problem-code',
   templateUrl: './problem-code.component.html',
-  styleUrls: ['./problem-code.component.css']
+  styleUrls: ['./problem-code.component.css'],
 })
 export class ProblemCodeComponent {
+  @Input() code!: string;
 
+  sanitizedCode!: SafeHtml;
+
+  constructor(private sanitizer: DomSanitizer) {}
+
+  onCodeChanged(value: any) {
+    console.log('CODE', value);
+  }
 }
